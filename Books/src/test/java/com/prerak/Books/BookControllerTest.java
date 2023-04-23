758303a018b372f70dbd76f03b6886d5d082fa4e
Prerak Choksi <prerakchoksi@preraks-mbp.home>
2023-04-22 21:59:52 -0400

    Added actuator and modify test appropriately

--- Books/src/test/java/com/prerak/Books/BookControllerTest.java
+++ Books/src/test/java/com/prerak/Books/BookControllerTest.java
@@ -3,16 +3,25 @@ package com.prerak.Books;
 import com.prerak.Books.controller.BookController;
 import com.prerak.Books.entity.Book;
 import com.prerak.Books.service.BookService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -20,22 +29,38 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-@WebMvcTest(BookController.class)
-public class BookControllerTest {
-    @Autowired
-    MockMvc mockMvc;
-    @MockBean
-    BookService bs;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+
+public class BookControllerTest {
+
+    @Mock
+    BookService bs;
     @InjectMocks
     BookController bc;
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(bc).build();
+    }
+
     @Test
     public void testGetAllBook() throws Exception {
-        Mockito.when(bs.getAllBook()).thenReturn(Arrays.asList(new Book("Book1", "B1", "A1")));
-        mockMvc.perform(get("/books")).andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()").value(1))
-                .andDo(print());;
+
+        Mockito.when(bs.getAllBook())
+                .thenReturn(Arrays.asList(new Book("Book1", "B1", "A1")));
+
+
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(1));
+        ;
 
     }
 }
